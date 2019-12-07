fd35f37706b3f35879aa159c2c0a3e0a45610aa9
drave <r3designs@gmail.com>
2019-12-07 01:40:31 +0100

    Add images to modals

--- src/components/Blessed.vue
+++ src/components/Blessed.vue
@@ -2,41 +2,34 @@
   <section class="blessed-dice">
     <b-modal
       :active="true"
-      has-modal-card
       trap-focus
       aria-role="dialog"
       aria-modal
+      full-screen
+      :can-cancel="false"
     >
-      <div class="modal-card" style="width: auto">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Blessed by the gods</p>
-        </header>
-        <section class="modal-card-body">
-          <p class="intro">
-            The Dark Gods will gift their most favoured Champions<br />
-            with boons of unnatural mutation. However, too many<br />
-            blessing can lead to damnation of a different kind -<br />
-            devolution into a Chaos Spawn, just ask Scylla!****
-          </p>
-          <h3 class="title is-3">The vagaries of fate</h3>
-          <p>
-            These bonuses are permanent. However, if you roll a<br />
-            Chaos Boon/Mutation that you already have (other than<br />
-            a Random Mutation), you immediately devolve into a<br />
-            Chaos Spawn and your journey end in ignominy!
-          </p>
-          <div class="container has-text-centered dice">
-            <Die :roll="DieOne"></Die>
+      <div class="container container--main is-fullhd">
+        <div class="columns">
+          <div class="column is-half is-offset-one-quarter">
+            <p class="image is-1by2">
+              <img
+                src="../assets/blessings.jpg"
+                alt="The vagaries of fate - The Dark Gods will gift their most favoured Champions with boons of unnatural mutation. However, too many blessing can lead to damnation of a different kind - devolution into a Chaos Spawn, just ask Scylla!****"
+              />
+            </p>
+            <div class="container has-text-centered dice">
+              <Die :roll="DieOne"></Die>
+            </div>
           </div>
-        </section>
-        <footer class="modal-card-foot">
-          <div class="container has-text-centered">
-            <button class="button is-primary" type="button" @click="roll">
-              Roll dice
-            </button>
-          </div>
-        </footer>
+        </div>
       </div>
+      <nav class="navbar is-fixed-bottom">
+        <div class="navbar-item">
+          <button class="button is-primary" type="button" @click="roll">
+            Roll a D6
+          </button>
+        </div>
+      </nav>
     </b-modal>
   </section>
 </template>
@@ -64,8 +57,10 @@ export default class Blessed extends Vue {
   display: block;
   margin: 20px 0 0;
 }
-.intro {
-  font-size: 1.1em;
-  margin-bottom: 20px;
+.navbar {
+  justify-content: center;
+}
+.container--main {
+  padding-bottom: 52px;
 }
 </style>
--- src/components/Injured.vue
+++ src/components/Injured.vue
@@ -2,33 +2,31 @@
   <section class="injury-dice">
     <b-modal
       :active="true"
-      has-modal-card
       trap-focus
       aria-role="dialog"
       aria-modal
+      full-screen
+      :can-cancel="false"
     >
-      <div class="modal-card" style="width: auto">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Injured in battle</p>
-        </header>
-        <section class="modal-card-body">
-          <p class="intro">
-            Life in the Mortal Realms is hazardous at best and downright<br />
-            lethal at worst. Add trying to impress the Dark Gods into the<br />
-            mix, and it's a recipe for death and dismemberment!
-          </p>
-          <div class="container has-text-centered dice">
-            <Die :roll="DieOne"></Die>
+      <div class="container container--main is-fullhd">
+        <div class="columns">
+          <div class="column is-half is-offset-one-quarter">
+            <p class="image is-1by2">
+              <img
+                src="../assets/injuries.jpg"
+                alt="Life in the Mortal Realms is hazardous at best and downright lethal at worst. Add trying to impress the Dark Gods into the mix, and it's a recipe for death and dismemberment!"
+              />
+            </p>
           </div>
-        </section>
-        <footer class="modal-card-foot">
-          <div class="container has-text-centered">
-            <button class="button is-primary" type="button" @click="roll">
-              Roll dice
-            </button>
-          </div>
-        </footer>
+        </div>
       </div>
+      <nav class="navbar is-fixed-bottom">
+        <div class="navbar-item">
+          <button class="button is-primary" type="button" @click="roll">
+            Roll a D6
+          </button>
+        </div>
+      </nav>
     </b-modal>
   </section>
 </template>
@@ -56,8 +54,10 @@ export default class Injured extends Vue {
   display: block;
   margin: 20px 0 0;
 }
-.intro {
-  font-size: 1.1em;
-  margin-bottom: 20px;
+.navbar {
+  justify-content: center;
+}
+.container--main {
+  padding-bottom: 52px;
 }
 </style>
